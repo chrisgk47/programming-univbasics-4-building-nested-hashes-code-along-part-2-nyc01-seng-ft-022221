5648cc3c9a754214b0493ec6a07db82ef37b849d
Christopher Kim <chris.kim1990@gmail.com>
2021-02-10 19:55:19 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,6 +50,10 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
+      :additional_characters => {
+         :name => "Prince Escalus",
+         :Apothecary => {}
+      }
    }
 
    # Write code below that does the following programmatically:
